65aa3c120489640d3a3ea05fc6120c4934ad97ac
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 19:12:25 +1000

    Update autosploit.sh

--- autosploit.sh
+++ autosploit.sh
@@ -39,7 +39,7 @@ echo -e "\e[0m"
 	
 echo -e "\e[34m\e[5m\e[1mPlease Wait.."
 echo -e "\e[0m"
-xsser -i /root/Bug-Bounty-Tools/bounty-targets-data/data/domains.txt --auto --reverse-check --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+xsser -i /root/Bug-Bounty-Tools/domains.txt --auto --reverse-check --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
 echo -e "\e[1m"
 echo -e "\e[93m"
 echo -e "\e[0m"
