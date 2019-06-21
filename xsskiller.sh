c58e27f31d6516fc778ae814cfbb95c754043078
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 17:22:21 +1000

    Update xsskiller.sh

--- xsskiller.sh
+++ xsskiller.sh
@@ -12,40 +12,78 @@ __   __ _____ _____ _  ___ _ _
 """
 
 echo -e "\e[1m"
-echo -e "\e[3m\e[35m[+] Scanning for subdomains, Yeet!! "
-echo -e "\e[32m[+] This will take sometime.."
-echo -e "\e[0m"
+echo -e "\e[94m Do you want to do a new scan [Y/n]: "
+read $scans
 
-cp bounty-targets-data/data/wildcards.txt .; cat wildcards.txt | sed 's/^*.//g' | grep -v '*' > wildcards_without_stars.txt; 
+if [ "$scans" = "Yes" ] || [ "$scans" = "Y" ] || [ "$scans" = "YES" ]; then
 
-while read host; 
-   do file=$host && file+="_subfinder.out"; 
-   ~/go/bin/subfinder -o $file -d $host -silent; 
 
-done < wildcards_without_stars.txt;
-cat *.out > all_subdomains.lst; 
+	echo -e "\e[1m"
+	echo -e "\e[3m\e[35m[+] Scanning for subdomains, Yeet!! "
+	echo -e "\e[32m[+] Could take sometime.."
+	echo -e "\e[0m"
 
-echo -e "\e[1m"
-echo -e "\e[5m\e[34m Appending https:// to each subdomain.."
 
-while read $file;
+	cp bounty-targets-data/data/wildcards.txt .; cat wildcards.txt | sed 's/^*.//g' | grep -v '*' > wildcards_without_stars.txt; 
+
+	while read host; 
+   	do file=$host && file+="_subfinder.out"; 
+   		~/go/bin/subfinder -o $file -d $host -silent; 
+
+	done < wildcards_without_stars.txt;	
+	cat *.out > all_subdomains.lst; 
+
+	echo -e "\e[1m"
+	echo -e "\e[3m\e[91m Appending https:// to each subdomain.."
+	echo -e "\e[0m"
+	while read $file;
 	do 
 		echo "https://"$h >> subdomins.txt 
-done < all_subdomains.lst;
+	done < all_subdomains.lst;
 
 
-xsser -i subdomains.lst --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigi$
+	xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+	echo -e "\e[1m"
+	echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
+	echo -e "\e[0m"
+	
+	echo -e "\e[34m\e[5m\e[1mPlease Wait.."
+	echo -e "\e[0m"
+	xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
 
-echo -e "\e[1m"
-echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
-echo -e "\e[0m"
+	echo -e "\e[1m"
+	echo -e "\e[93m"
+	cat *.raw >> report.raw
+	cat report.raw
+	echo -e "\e[0m"
 
-echo -e "\e[34m\e[5m\e[1mPlease Wait.."
-echo -e "\e[0m"
-xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
 
-echo -e "\e[1m"
-echo -e "\e[93m"
-cat *.raw >> report.raw
-cat report.raw
-echo -e "\e[0m"
+else
+	
+        cat *.out > all_subdomains.lst; 
+
+	echo -e "\e[1m"
+	echo -e "\e[3m\e[91m Appending https:// to each subdomain.."
+	echo -e "\e[0m"
+	while read $file;
+	do 
+		echo "https://"$h >> subdomins.txt 
+	done < all_subdomains.lst;
+
+
+	xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+	echo -e "\e[1m"
+	echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
+	echo -e "\e[0m"
+	
+	echo -e "\e[34m\e[5m\e[1mPlease Wait.."
+	echo -e "\e[0m"
+	xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+
+	echo -e "\e[1m"
+	echo -e "\e[93m"
+	cat *.raw >> report.raw
+	cat report.raw
+	echo -e "\e[0m"
+
+fi
