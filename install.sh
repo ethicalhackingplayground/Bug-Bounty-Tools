42fbbd287b8556f13708bc81689703e6eef562dc
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 13:38:06 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,11 @@ echo -e "\e[32m[+] Installing Xsser"
 echo -e "\e[0m"
 apt-get install xsser
 
+echo -e "\e[1m"
+echo -e "\e[32m[+] Downloading sunfinder.."
+echo -e "\e[0m"
+
+apt install golang;
+go get github.com/subfinder/subfinder;
+
 
