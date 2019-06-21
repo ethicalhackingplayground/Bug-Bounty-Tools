25305498df53a16c99ff0fcdac1b9e323a088b4f
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 19:15:41 +1000

    Update xsskiller.sh

--- xsskiller.sh
+++ xsskiller.sh
@@ -44,14 +44,14 @@ if [ "$scans" = "Yes" ] || [ "$scans" = "Y" ] || [ "$scans" = "YES" ] || [ "$sca
 	done < all_subdomains.lst;
 
 
-	xsser -i /root/Bug-Bounty-Tools/subdomains.lst --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+	xsser -i /root/Bug-Bounty-Tools/subdomains.lst --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 
 	echo -e "\e[1m"
 	echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
 	echo -e "\e[0m"
 	
 	echo -e "\e[34m\e[5m\e[1mPlease Wait.."
 	echo -e "\e[0m"
-	xsser -i /root/Bug-Bounty-Tools/domains.txt --auto --reverse-check --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+	xsser -i /root/Bug-Bounty-Tools/domains.txt --auto --reverse-check --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 
 
 	echo -e "\e[1m"
 	echo -e "\e[93m"
@@ -73,13 +73,13 @@ else
 	done < all_subdomains.lst;
 
 
-	xsser -i /root/Bug-Bounty-Tools/subdomains.lst --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+	xsser -i /root/Bug-Bounty-Tools/subdomains.lst --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10
 	echo -e "\e[1m"
 	echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
 	echo -e "\e[0m"
 	
 	echo -e "\e[34m\e[5m\e[1mPlease Wait.."
 	echo -e "\e[0m"
-	xsser -i /root/Bug-Bounty-Tools/domains.txt --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+	xsser -i /root/Bug-Bounty-Tools/domains.txt --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 
 
 fi
