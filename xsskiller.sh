37c383c06c543251890f958a02dfd32c7c539601
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 14:02:36 +1000

    Update xsskiller.sh

--- xsskiller.sh
+++ xsskiller.sh
@@ -4,4 +4,4 @@ echo -e "\e[1m"
 echo -e "\e[93m[+] Running XSS on all domains, Yeet!! "
 echo -e "\e[0m"
 
-xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=10 --silent --save 
+xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=10 --silent --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10  --delay=1
