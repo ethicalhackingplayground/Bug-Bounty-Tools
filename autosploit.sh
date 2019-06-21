2a2ae7c7dd833da39b94125e980a991cec708a37
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 19:11:08 +1000

    Update autosploit.sh

--- autosploit.sh
+++ autosploit.sh
@@ -32,14 +32,14 @@ echo -e "\e[1m"
 echo -e "\e[93m[+] Running XSS on all <subdomains>, Yeet!! "
 echo -e "\e[0m"
 
-xsser -i /root/Bug-Bounty-Tools/subdomains.lst --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+xsser -i /root/Bug-Bounty-Tools/subdomains.lst --auto --reverse-check --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
 echo -e "\e[1m"
 echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
 echo -e "\e[0m"
 	
 echo -e "\e[34m\e[5m\e[1mPlease Wait.."
 echo -e "\e[0m"
-xsser -i /root/Bug-Bounty-Tools/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+xsser -i /root/Bug-Bounty-Tools/bounty-targets-data/data/domains.txt --auto --reverse-check --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
 echo -e "\e[1m"
 echo -e "\e[93m"
 echo -e "\e[0m"
