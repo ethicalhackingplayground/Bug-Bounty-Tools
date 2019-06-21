59a130a8dfd85705462095d73aebb7be2faeb378
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 15:45:33 +1000

    Update xsskiller.sh

--- xsskiller.sh
+++ xsskiller.sh
@@ -11,11 +11,35 @@ __   __ _____ _____ _  ___ _ _
 /_/ \_\_____/_____/|_|\_\_|_|_|\___|_| 
 """
 
+echo -e "\e[1m"
+echo -e "\e[93m[+] Running XSS on all <subdomains>, Yeet!! "
+echo -e "\e[0m"
+
+
+cp bounty-targets-data/data/wildcards.txt .; cat wildcards.txt | sed 's/^*.//g' | grep -v '*' > wildcards_without_stars.txt; 
+
+while read host; 
+   do file=$host && file+="_subfinder.out"; 
+   ~/go/bin/subfinder -o $file -d $host; 
+
+done < wildcards_without_stars.txt;
+cat *.out > all_subdomains.lst; 
 
 echo -e "\e[1m"
-echo -e "\e[93m[+] Running XSS on all domains, Yeet!! "
+echo -e "\e[34m Appending https:// to each subdomain.."
+
+while read $file;
+	do 
+		echo "https://"$h >> subdomins.txt 
+done < all_subdomains.lst;
+
+
+xsser -i subdomains.lst --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigi$
+
+echo -e "\e[1m"
+echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
 echo -e "\e[0m"
 
 echo -e "\e[34m\e[5m\e[1mPlease Wait.."
 echo -e "\e[0m"
-xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=10 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+xsser -i /root/xsskiller/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
