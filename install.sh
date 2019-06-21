bd744e601f8ba4241d617ddfda68486593783f44
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 17:39:32 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,19 @@
 echo -e "\e[1m"
 echo -e "\e[32m[+] Acquiring Targets.."
 echo -e "\e[0m"
+cd ~;
 git clone https://github.com/arkadiyt/bounty-targets-data;
 
 echo -e "\e[1m"
-echo -e "\e[32m[+] Installing Xsser"
-echo -e "\e[0m"
-apt-get install xsser
-
-echo -e "\e[1m"
-echo -e "\e[32m[+] Downloading sunfinder.."
+echo -e "\e[32m[+] Downloading subfinder.."
 echo -e "\e[0m"
 
 apt install golang;
 go get github.com/subfinder/subfinder;
 
+echo -e "\e[1m"
+echo -e "\e[32m[+] Downloading SubOver.."
+echo -e "\e[0m"
+
+go get github.com/Ice3man543/SubOver
 
