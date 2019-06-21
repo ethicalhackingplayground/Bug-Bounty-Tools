9c2abab12bdb6040f6ba280146faf9880517d868
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-06-21 19:13:45 +1000

    Update xsskiller.sh

--- xsskiller.sh
+++ xsskiller.sh
@@ -67,25 +67,19 @@ else
 	echo -e "\e[1m"
 	echo -e "\e[3m\e[91m Appending https:// to each subdomain.."
 	echo -e "\e[0m"
-	while read $file;
+	while read h;
 	do 
-		echo "https://"$h >> subdomins.txt 
+		echo "https://"$h >> subdomains.lst 
 	done < all_subdomains.lst;
 
 
-	xsser -i /root/Bug-Bounty-Tools/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
+	xsser -i /root/Bug-Bounty-Tools/subdomains.lst --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
 	echo -e "\e[1m"
 	echo -e "\e[93m[+] Running XSS on all <domains>, Yeet!! "
 	echo -e "\e[0m"
 	
 	echo -e "\e[34m\e[5m\e[1mPlease Wait.."
 	echo -e "\e[0m"
-	xsser -i /root/Bug-Bounty-Tools/bounty-targets-data/data/domains.txt --auto --Str --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
-
-	echo -e "\e[1m"
-	echo -e "\e[93m"
-	cat *.raw >> report.raw
-	cat report.raw
-	echo -e "\e[0m"
+	xsser -i /root/Bug-Bounty-Tools/domains.txt --auto --Str --reverse-check --Coo --Xsa --Xsr --Ind --Anchor --Dcp --Dom -c 100 --Cw=50 --delay=1 --save  --Phpids0.6.5  --Phpids0.7 --Imperva --Webknight --F5bigip --Barracuda --Modsec --Quickdefense --heuristic --threads=10 --silent
 
 fi
